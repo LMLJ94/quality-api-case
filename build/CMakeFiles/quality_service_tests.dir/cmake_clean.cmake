2d35b5a149b7f95f94283219449c5ca35dd417e7
Louise MLJ <lmlj76232@edu.ucl.dk>
2026-04-13 13:39:22 +0200

    Initial commit - imported case project

---
+++ build/CMakeFiles/quality_service_tests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quality_service_tests.dir/tests/quality_service_test.cpp.obj"
+  "CMakeFiles/quality_service_tests.dir/tests/quality_service_test.cpp.obj.d"
+  "libquality_service_tests.dll.a"
+  "quality_service_tests.exe"
+  "quality_service_tests.exe.manifest"
+  "quality_service_tests.pdb"
+  "quality_service_tests[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quality_service_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
