9923a1b8ed83abeea8876f6c4921b3e2ddbff48e
Louise MLJ <lmlj76232@edu.ucl.dk>
2026-04-14 10:50:56 +0200

    Fix quality service implementation and tests 3

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Rumle/Downloads/itt-cicd-case-project-main/itt-cicd-case-project-main
-# Build directory: C:/Users/Rumle/Downloads/itt-cicd-case-project-main/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Rumle/Downloads/itt-cicd-case-project-main/build/quality_service_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
